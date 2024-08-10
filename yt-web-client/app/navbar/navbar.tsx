1e8b36a05f298da72fe67b86fe806d2dcb2668fa
Mansoor <mansoorlunawadi@yahoo.ca>
2024-08-10 14:06:32 -0400

    Added google sign in button

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,14 +1,34 @@
+'use client';
+
 import Image from "next/image"; //react Image element
 import Link from "next/link"
 import styles from "./navbar.module.css"
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+    // Init user state
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // stop watching authentication state if the component dies
+        return () => unsubscribe();
+    });
     return (
         <nav className={styles.nav}>
             <Link href="/">
                 <Image width = {90} height = {20}
                     src = "/youtube-logo.svg" alt = "YouTube Logo" />
             </Link>
+            {
+                // TODO: Add a upload button IF user is signed in --> need onauthstatechanged helper here
+            }
+            <SignIn user={user}/>
         </nav>
     );
 }
